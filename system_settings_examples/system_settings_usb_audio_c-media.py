7350d5738b10bf9ee8cd3f2696892a3a7f994cbf
sindar <godzilla@chaos.tokyo>
2020-10-26 21:16:24 +0300

    Add USB Audio system settings example

---
+++ system_settings_examples/system_settings_usb_audio_c-media.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# project: pRodriguezAssistant
+
+RECORD_MIXER = 'Mic'
+RECORD_DEVICE = None #example: plughw:1,0
+
+SPEAKER_MIXER = 'Speaker'
+#The scale of the speaker voulme modes and step depends on device
+SPEAKER_MODES = {
+    'min': 0,
+    'quiet': 8,
+    'normal': 20,
+    'loud': 32,
+    'max': 40
+}
+
+VOLUME_STEP = (SPEAKER_MODES['max'] - SPEAKER_MODES['min']) / 10
